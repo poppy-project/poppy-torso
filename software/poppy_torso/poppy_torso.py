b69df826f4cd0897523f586efcc97dc96fc9dcc6
Théo Segonds <theo.segonds@inria.fr>
2015-06-16 20:41:36 +0200

    Avoid motor block with safe compliance at startup

--- software/poppy_torso/poppy_torso.py
+++ software/poppy_torso/poppy_torso.py
@@ -7,35 +7,44 @@ from poppy.creatures import AbstractPoppyCreature
 
 from .primitives.safe import LimitTorque, TemperatureMonitor
 from .primitives.dance import SimpleBodyBeatMotion
-#from .primitives.posture import StandPosition, SitPosition
 from .primitives.idle import UpperBodyIdleMotion, HeadIdleMotion
 from .primitives.interaction import ArmsTurnCompliant, PuppetMaster
 
 
 class PoppyTorso(AbstractPoppyCreature):
+
     @classmethod
     def setup(cls, robot):
         robot._primitive_manager._filter = partial(numpy.sum, axis=0)
 
         if robot.simulated:
             cls.vrep_hack(robot)
-        for m in robot.motors:
-            m.compliant_behavior = 'safe'
-            m.goto_behavior = 'minjerk'
 
+        for m in robot.motors:
+            m.goto_behavior = 'minjerk'
+            # Avoid motor block with safe compliance at startup
+            if m.present_position < min(m.angle_limit):
+                m.compliant = False
+                m.goto_position(min(m.angle_limit) + 10, 0.5, wait=True)
+                m.compliant = True
+
+            if m.present_position > max(m.angle_limit):
+                m.compliant = False
+                m.goto_position(max(m.angle_limit) - 10, 0.5, wait=True)
+                m.compliant = True
+
+            m.compliant_behavior = 'safe'
 
         robot.attach_primitive(LimitTorque(robot, torque_max=80), 'limit_torque')
-        #robot.limit_torque.start()
-       
-        # Temperature monitoring 
+        # robot.limit_torque.start()
+
+        # Temperature monitoring
         sound_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                   'media', 'sounds', 'error.wav')
         robot.attach_primitive(TemperatureMonitor(robot, sound=sound_file), 'temperature_monitoring')
         robot.temperature_monitoring.start()
 
         robot.attach_primitive(SimpleBodyBeatMotion(robot, 50), 'dance_beat_motion')
-
-        
 
         # Idle primitives
         robot.attach_primitive(UpperBodyIdleMotion(robot, 50), 'upper_body_idle_motion')
