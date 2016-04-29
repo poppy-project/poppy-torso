8daa9f1026b892f18127c3361f6631ee95cbafcb
Matthieu Lapeyre <matthieulapeyre@gmail.com>
2016-04-29 15:19:43 +0200

    add small offset on the bust for reducing the torque applied

--- software/poppy_torso/primitives/posture.py
+++ software/poppy_torso/primitives/posture.py
@@ -13,5 +13,6 @@ class InitPosition(pypot.primitive.Primitive):
         self.robot.power_up()
 
         self.robot.goto_position(dict(zip((m.name for m in self.robot.motors), itertools.repeat(0))), 3, wait=True)
+        self.robot.bust_y.goto_position(4, 1)
 
         time.sleep(0.5)
