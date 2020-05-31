82eeea7d4b17b24017f2d939ef1a35bf849da893
Yoan Mollard <ymollard@users.noreply.github.com>
2020-05-31 04:35:50 +0200

    pypot >= 3.2.0

--- software/setup.py
+++ software/setup.py
@@ -20,7 +20,7 @@ setup(name='poppy-torso',
       version=version(),
       packages=find_packages(),
 
-      install_requires=['pypot >= 3.0'],
+      install_requires=['pypot >= 3.2.0'],
 
       include_package_data=True,
       exclude_package_data={'': ['README', '.gitignore']},
