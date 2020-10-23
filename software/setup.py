c26fde0b5237c2e51494ee5bc2e3da359cb2b7a1
Yoan Mollard <ymollard@users.noreply.github.com>
2020-10-23 11:26:46 +0200

    pypot 4 + dropped 2to3 + author

--- software/setup.py
+++ software/setup.py
@@ -6,32 +6,24 @@ import sys
 
 from setuptools import setup, find_packages
 
-
 def version():
     with open('poppy_torso/_version.py') as f:
         return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read()).group(1)
-
-
-extra = {}
-if sys.version_info >= (3,):
-    extra['use_2to3'] = True
 
 setup(name='poppy-torso',
       version=version(),
       packages=find_packages(),
 
-      install_requires=['pypot >= 3.2.0'],
+      install_requires=['pypot >= 4.0.0'],
 
       include_package_data=True,
       exclude_package_data={'': ['README', '.gitignore']},
 
       zip_safe=False,
 
-      author='Pierre Rouanet, Matthieu Lapeyre',
-      author_email='pierre.rouanet@gmail.com',
+      author='Poppy Station',
+      author_email='dev@poppy-station.org',
       description='Poppy Torso Software Library',
-      url='https://github.com/poppy-project/poppy-creature',
+      url='http://poppy-project.org',
       license='GNU GENERAL PUBLIC LICENSE Version 3',
-
-      **extra
       )
